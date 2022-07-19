f26687db356e208754a269b0ad43aca6298a77dd
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-07-19 01:51:43 -0700

    adding python route to flask application script

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""script starts Flask web application and defines route '/'"""
+
+from flask import Flask
+# create Flask application instance, passing it the name of the current module
+app = Flask(__name__)
+
+
+# decorator turns a python function into a Flask view function
+@app.route('/', strict_slashes=False)
+def hello():
+    """funct responds to requests to the main URL diplaying 'Hello HBNB'"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """defining behavior for /hbnb url, display 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """defining /c url behavior w dynamic capacity to display text"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """define /python/<text> URL behavior"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+# start the flask application listening on host address and ports specificied
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
