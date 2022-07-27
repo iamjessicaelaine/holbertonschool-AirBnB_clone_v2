15fdac670db6364e4cfff20b92c561a8052ca6c1
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-07-26 23:07:19 -0700

    update to city to add foreign key and state model update for storage conditions

--- models/city.py
+++ models/city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Foreign Key
 
 
 # city needs to inherit from base as part of the mapping process
@@ -10,4 +10,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
 
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), nullable=False)
+    state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
--- models/state.py
+++ models/state.py
@@ -2,10 +2,29 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from os import getenv
+from models.city import City
+from sqlalchemy.orm import relationship
+from models import storage
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-
     name = Column(String(128), nullable=False)
+
+    storagenv = getenv('HBNB_TYPE_STORAGE')
+
+    if storagenv == 'db':
+        # define relationship between city and state
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
+    elif storagenv == 'file':
+        @property  # prepare a getter attr to define rltshp of city & state
+        def cities(self):
+            """returns list of City instances with current state.id"""
+            citylist = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    citylist.append(city)
+            return citylist
