c0aee1dd39a35644c6cb95eb1401ec63a9d74a9e
iamjessicaelaine <95127791+iamjessicaelaine@users.noreply.github.com>
2022-07-27 01:23:24 -0500

    Update state.py

--- models/state.py
+++ models/state.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, String
 from os import getenv
 from models.city import City
 from sqlalchemy.orm import relationship
-from models import storage
+import storage
 
 
 class State(BaseModel, Base):
@@ -13,9 +13,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
-    storagenv = getenv('HBNB_TYPE_STORAGE')
-
-    if storagenv == 'db':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         # define relationship between city and state
         cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
